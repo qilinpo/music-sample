5dcc4f0fcafd7858d095ce12c95e84c28d76c942
PQL <qilinpo312@gmail.com>
2022-07-22 22:13:10 +0800

    feat: read from asset branch

--- src/handlers.ts
+++ src/handlers.ts
@@ -1,26 +1,25 @@
-import { Request, Response } from "express";
-import { createReadStream, readdir } from "fs";
+import { Request, response, Response } from "express";
+import fs from "fs";
+import https from "https";
 
 export const rootHandler = (_req: Request, res: Response) => {
   return res.send("API is working");
 };
 
 export const audioHandler = (_req: Request, res: Response) => {
-  readdir("./assets/", (err, files) => {
-    if (err) return res.send(err.message);
-    const audioList: string[] = files.map((file) => `./assets/${file}`)
-    const audioFilename: string =
-      audioList[Math.floor(Math.random() * audioList.length)];
+  const audioList: string[] = [
+    "https://raw.githubusercontent.com/qilinpo/music-sample/assets/sample-01.mp3",
+    "https://raw.githubusercontent.com/qilinpo/music-sample/assets/sample-02.mp3",
+    "https://raw.githubusercontent.com/qilinpo/music-sample/assets/sample-03.mp3",
+  ];
+  const audioUrl: string =
+    audioList[Math.floor(Math.random() * audioList.length)];
 
-    const readStream = createReadStream(audioFilename);
-
-
-    readStream.on("open", function () {
-      readStream.pipe(res);
+  https.get(audioUrl, (response) => {
+    res.set({
+      "Content-Disposition": response.headers["content-disposition"],
+      "Content-Type": response.headers["content-type"],
     });
-
-    readStream.on("error", function (err) {
-      res.end(err);
-    });
+    res.end()
   });
 };
