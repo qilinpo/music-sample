83b0633b8870ee09d481ebeee9daefb0e980ef48
PQL <qilinpo312@gmail.com>
2022-07-22 14:13:08 +0800

    refactor: rename audio files

--- src/handlers.ts
+++ src/handlers.ts
@@ -4,7 +4,9 @@ import https from "https"
 type AudioUrlList = string[];
 
 const audioList: AudioUrlList = [
-  "https://github.com/qilinpo/music-sample/blob/main/pokemon-battle.mp3?raw=true",
+  "https://github.com/qilinpo/music-sample/blob/main/assets/sample-01.mp3?raw=true",
+  "https://github.com/qilinpo/music-sample/blob/main/assets/sample-02.mp3?raw=true",
+  "https://github.com/qilinpo/music-sample/blob/main/assets/sample-03.mp3?raw=true",
 ];
 
 export const rootHandler = (_req: Request, res: Response) => {
